01544936df0e64136c347824299d968ee64c3864
Roy <92612927+PetarWho@users.noreply.github.com>
2023-03-06 22:57:45 +0200

    Revert "Huge update"
    This reverts commit 65f67cb59166831ca04995589e31b2bc76abb290.

--- engine/abuser.py
+++ engine/abuser.py
@@ -1,23 +1,19 @@
 import pyautogui
 import asyncio
+import datetime
 import keyboard
-import time
 from colorama import Fore, Style
-import console_utils
-from engine_utils import current_time
 
 mining_category_location = [743, 319]
 cobble_location = [851, 321]
 enchanted_cobble_location = [1014, 371]
 sell_location = [850, 483]
 confirm_location = [850, 405]
-interval = console_utils.get_interval_input()
-global start_time
 
 
 async def click(x, y):
     pyautogui.moveTo(x, y)
-    await asyncio.sleep(0.2)
+    await asyncio.sleep(0.5)
     pyautogui.click()
 
 
@@ -30,44 +26,45 @@ async def send_command(command: str):
     pyautogui.press('enter')
 
 
-async def start_mining():
-    pyautogui.keyDown('ctrl')
-    pyautogui.keyDown('w')
-    pyautogui.mouseDown(button='left')
-
-
 async def stop_mining():
-    pyautogui.mouseUp(button='left')
-    pyautogui.keyUp('w')
-    pyautogui.keyUp('ctrl')
+    pyautogui.click()
+    pyautogui.press('f2')
+    pyautogui.press('f9')
+
+
+async def start_mining():
+    pyautogui.press('f9')
+    pyautogui.press('f10')
+    await asyncio.sleep(1)
+    pyautogui.press('f1')
+    pyautogui.click()
 
 
 async def close_menu():
     pyautogui.press('esc')
 
 
-def print_mine_time():
-    try:
-        end_time = time.time()
-        elapsed_time = end_time - start_time
-        if elapsed_time < 60:
-            print(f"{current_time()} You've been mining for {Fore.LIGHTMAGENTA_EX + str(round(elapsed_time, 1)) + Style.RESET_ALL} seconds")
-        elif elapsed_time < 3600:
-            print(f"{current_time()} You've been mining for {Fore.LIGHTMAGENTA_EX + str(round(elapsed_time/60, 1)) + Style.RESET_ALL} minute") if elapsed_time == 1 else print(f"{current_time()} You've been mining for {Fore.LIGHTMAGENTA_EX + str(round(elapsed_time/60, 1)) + Style.RESET_ALL} minutes")
-        else:
-            print(f"{current_time()} You've been mining for {Fore.LIGHTMAGENTA_EX + str(round(elapsed_time/3600, 1)) + Style.RESET_ALL} hour") if elapsed_time == 1 else print(f"{current_time()} You've been mining for {Fore.LIGHTMAGENTA_EX + str(round(elapsed_time/3600, 1)) + Style.RESET_ALL} hours")
-    except Exception:
-        pass
+def write(text: str, color=''):
+    print(color + text + Style.RESET_ALL)
+
+
+def current_time():
+    return f"{Fore.GREEN + '[' + datetime.datetime.now().strftime('%d %b, %I:%M %p')  + ']' + Style.RESET_ALL}"
+
+
+def print_mine_hours():
+    print(f"{current_time()} You've been mining for {Fore.LIGHTMAGENTA_EX + str(hours_spent) + Style.RESET_ALL} hour") if hours_spent == 1 else print(f"{current_time()} You've been mining for {Fore.LIGHTMAGENTA_EX + str(hours_spent) + Style.RESET_ALL} hours")
+
 
 async def abuse():
-    console_utils.write("Mining Starts in 5 secs...\n", Fore.LIGHTYELLOW_EX)
+    write("Mining Starts in 5 secs...\n", Fore.LIGHTYELLOW_EX)
     await asyncio.sleep(6)
-    global start_time
-    start_time = time.time()
     print(f"{ current_time()} Mining started!")
+    global hours_spent
+    hours_spent = 1
     while True:
         await start_mining()
-        await asyncio.sleep(interval)
+        await asyncio.sleep(3600)
         await stop_mining()
         await asyncio.sleep(0.5)
         await send_command("bz")
@@ -84,13 +81,14 @@ async def abuse():
         await asyncio.sleep(1)
         await close_menu()
         await asyncio.sleep(1)
+        print_mine_hours()
+        hours_spent += 1
 
 
 async def terminate(task):
     while True:
-        if keyboard.is_pressed('z'):
+        if keyboard.is_pressed('q'):
             task.cancel()
-            await stop_mining()
             break
         await asyncio.sleep(0.1)
 
@@ -101,7 +99,6 @@ async def run():
         await asyncio.gather(task, terminate(task))
     except asyncio.CancelledError:
         print(f"\n{current_time()} Cobble Abuser 9000 has finished it's task...")
-        print_mine_time()
     keyboard.wait()
 
 
--- engine/engine_utils.py
+++
@@ -1,8 +0,0 @@
-from colorama import Fore, Style
-import datetime
-
-
-def current_time():
-    return f"{Fore.GREEN + '[' + datetime.datetime.now().strftime('%d %b, %I:%M %p')  + ']' + Style.RESET_ALL}"
-
-
